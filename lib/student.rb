03f083b22737ade9f329c720ea15cf8897373018
Roman Turner <rn.turner@live.com>
2021-04-08 23:47:09 -0700

    assignment complete

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User 
+    attr_accessor :knowledge 
+    
+    def initialize(knowledge = [])
+      @knowledge = knowledge
+    end
+
+    def learn(stuff)
+        @knowledge << stuff 
+    end
 
 end
